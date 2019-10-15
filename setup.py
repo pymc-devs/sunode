f05f92beabcd3d4ad40f97e44bda8c86595f7663
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2019-10-15 18:43:08 +0200

    Rename to sunode

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='pysundials-cffi',
+    name='sunode',
     version='0.0.1',
     author='Adrian Seyboldt',
     author_email='adrian.seyboldt@gmail.com',
     description='Python wrapper of sundials for solving ordinary differential equations',
-    url='https://github.com/aseyboldt/pysundials_cffi',
+    url='https://github.com/aseyboldt/sunode',
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=[
-        #"pysundials_cffi/build_cvode.py:ffibuilder",
-        "pysundials_cffi/build_cvodes.py:ffibuilder",
+        "sunode/build_cvodes.py:ffibuilder",
     ],
     install_requires=[
         "cffi>=1.0.0",
