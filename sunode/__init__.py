48b7e61e2ae2e0c6454db92b9c8bb71b17a73b27
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2020-06-10 15:17:26 +0200

    Export SympyProblem at top level

--- sunode/__init__.py
+++ sunode/__init__.py
@@ -2,12 +2,13 @@ import _sundials_cvodes as _cvodes  # type: ignore
 
 from sunode.vector import empty_vector, from_numpy
 from sunode.matrix import empty_matrix
-from sunode import solver
+from sunode.symode import SympyProblem
+import sunode.solver
 
 __all__ = [
     "empty_matrix",
     "empty_vector",
     "from_numpy",
     "solve_ode",
-    "_cvodes",
+    "SympyProblem",
 ]
